7d8d919a0567e1c41571a47d529ef053cf01d641
Victor <67214928+VictorrPY@users.noreply.github.com>
2020-09-08 12:59:31 -0300

    Deleted Auto Exit and Added Better GUI

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import ctypes
 import requests
 from requests import get
 
-
-ctypes.windll.kernel32.SetConsoleTitleW("Discord Webhook Sender v.1.1 | Made by github.com/VictorrPY")
+ctypes.windll.kernel32.SetConsoleTitleW("Discord Webhook Sender v1.2 | Made by github.com/VictorrPY")
 init()
 
-version = str(1.1)
+version = str(1.2)
 current_version = str(get(url="https://pastebin.com/raw/rkyrGt8V").text)
 
 with open("config.json", "r") as f:
@@ -21,14 +20,6 @@ url = data["webhook"]
 color = data["console-color"].lower()
 name = data["username"]
 avatar = data["avatar"]
-exit = data["autoexit"].lower()
-enable_embed = data["enable-embed"].lower()
-embed = data["embed"]
-embed_title = data["embed"]["title"]
-embed_description = data["embed"]["description"]
-embed_image = data["embed"]["image"]
-embed_thumbnail = data["embed"]["thumbnail"]
-embed_footer = data["embed"]["footer"]
 
 if color == "blue":
   fore = Fore.BLUE
@@ -55,75 +46,90 @@ print(fore + '''
 
 download = 0
 if current_version != version:
-    print("YOUR VERSION IS OUTDATED, WOULD YOU LIKE TO DOWNLOAD THE NEW VERSION??")
+    pass
+else:
+    print("YOUR VERSION IS OUTDATED, WOULD YOU LIKE TO DOWNLOAD THE NEW VERSION?")
     download = input("[1] Yes\n[2] No\nSelect your option: ")
     while download not in ["1", "2"]:
         downlaod = input("Please select a correct option: ")
-else:
-    pass
 if download == "1":
     print("Downloading new version...\n")
-    url = f'https://github.com/VictorrPY/Discord-Webhook-Sender/releases/download/v{current_version}/DiscordWebhookSender.rar'
+    url = f'https://github.com/VictorrPY/Discord-Webhook-Sender/releases/download/v{current_version}/DiscordWebhookSender.zip'
     r = requests.get(url, allow_redirects=True)
-    open(f'DiscordWebhookSender v{current_version}.rar', 'wb').write(r.content)
+    open(f'DiscordWebhookSender v{current_version}.zip', 'wb').write(r.content)
+    
 else:
     pass
-
-print(fore + "Welcome to Victor's Webhook Sender")
-print(fore + "[ANNOUNCEMENT] I ADDED EMBED MESSAGES, LOOK CONFIG.JSON TO CONFIGURE IT.\n")
+print(fore + "\nWelcome to Victor's Webhook Sender")
 
 while True:
-  if enable_embed == "false":
-      if exit == "true":
+    option = input("[1] Text\n[2] Embed\n[3] Text and Embed\nPlease select an option: ")
+    while option not in ["1", "2", "3"]:
+        option = input("Please select an correct option: ")
 
-        text = input("Please write something for the Webhook to send: ")
+    if option == "1":
+
+        text = input("\nPlease write something for the Webhook to send: ")
 
         wb = Webhook(url, username=name, avatar_url=avatar)
+
         wb.send(text)
 
-        print(fore + f"\nText sent successfully with username: {name} and text: {text}\n")
-        time.sleep(1)
-        sys.exit()
+        print(fore + f"\nText sent successfully with Username: {name} and text: {text}\n")
 
-      elif exit == "false":
-         text = input("Please write something for the Webhook to send: ")
+        time.sleep(0.5)
+
+    elif option == "2":
+         print("\nIT DOESN'T MATTER IF YOU LEAVE AN EMPTY OPTION.")
+         time.sleep(0.5)
+         title = input("Select a Title text for the embed: ")
+         description = input("Select a Description text for the embed: ")
+         image = input("Select an Image for the embed (IT NEED TO BE AN URL WITH AN IMAGE): ")
+         thumbnail = input("Select an Thumbnail for the embed (IT NEED TO BE AN URL WITH AN IMAGE): ")
+         footer = input("Select an Footer text for the embed: ")
 
          wb = Webhook(url, username=name, avatar_url=avatar)
-         wb.send(text)
 
-         print(fore + f"\nText sent successfully with username: {name} and text: {text}\n")
-         time.sleep(1)
-  else:
-      if exit == "true":
-        text = input( "Please write something for the Webhook to send: ")
+         embed = Embed(title=title, description=description)
+         embed.set_image(image)
+         embed.set_thumbnail(thumbnail)
+         embed.set_footer(text=footer)
 
-        wb = Webhook(url, username=name, avatar_url=avatar)
+         wb.send(embed=embed)
 
-        embed = Embed(title=embed_title, description=embed_description)
-        embed.set_thumbnail(embed_thumbnail)
-        embed.set_footer(embed_footer)
-        embed.set_image(embed_image)
+         print(fore + f"\nEmbed sent successfully with Username: {name}\n")
 
-        wb.send(embed=embed)
-        wb.send(text)
-
-        print(fore + f"\nText sent successfully with username: {name} and text: {text}\n")
-        time.sleep(1)
-        sys.exit()
-
-      elif exit == "false":
-        text = input("Please write something for the Webhook to send: ")
+         time.sleep(0.5)
+    elif option == "3":
+        text = input("\nPlease write something for the Webhook to send: ")
+        time.sleep(0.5)
+        print("\nIT DOESN'T MATTER IF YOU LEAVE AN EMPTY OPTION.")
+        time.sleep(0.5)
+        title = input("Select a Title text for the embed: ")
+        description = input("Select a Description text for the embed: ")
+        image = input("Select an Image for the embed (IT NEED TO BE AN URL WITH AN IMAGE): ")
+        thumbnail = input("Select an Thumbnail for the embed (IT NEED TO BE AN URL WITH AN IMAGE): ")
+        footer = input("Select an Footer text for the embed: ")
 
         wb = Webhook(url, username=name, avatar_url=avatar)
 
-        embed = Embed(title=embed_title, description=embed_description)
-        embed.set_thumbnail(embed_thumbnail)
-        embed.set_footer(embed_footer)
-        embed.set_image(embed_image)
+        embed = Embed(title=title, description=description)
+        embed.set_image(image)
+        embed.set_thumbnail(thumbnail)
+        embed.set_footer(text=footer)
 
-        wb.send(embed=embed)
-        wb.send(text)
+        try:
+            wb.send(text)
+            wb.send(embed=embed)
+        except ValueError:
+            try:
+                wb.send(text)
+            except:
+                pass
+        if text == "":
+            pass
+        else:
+            print(fore + f"Text and Embed sent successfully with Username: {name} and Text: {text}\n")
+        time.sleep(0.5)
 
-        print(fore + f"\nText sent successfully with username: {name} and text: {text}\n")
-        time.sleep(1)
 
